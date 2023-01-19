e74954873c5648924500b7df282dfda22c47d3c4
dlebansais <dlebansais@gmail.com>
2023-01-19 16:25:00 +0100

    .

--- websocket/server/main.go
+++ websocket/server/main.go
@@ -4,7 +4,11 @@ import (
 	"flag"
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/websocket"
 )
+
+var upgrader = websocket.Upgrader{}
 
 func main() { // nolint:gocognit
 	answerAddr := flag.String("answer-address", ":22570", "Address that the Answer HTTP server is listening on.")
@@ -12,8 +16,34 @@ func main() { // nolint:gocognit
 
 	fmt.Printf("Answer address: %s\n", *answerAddr)
 
+	// A HTTP handler that processes a connection request
+	http.HandleFunc("/connect", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Printf("Request on /connect\n")
+
+		socketConn, err := upgrader.Upgrade(w, r, nil)
+		if err != nil {
+			panic(err)
+		}
+
+		go HandleClient(socketConn)
+
+		fmt.Printf("Request on /connect has been handled\n")
+	})
+
 	fmt.Printf("Starting server\n")
 
 	// Start HTTP server that accepts requests from the offer process to exchange SDP and Candidates
 	panic(http.ListenAndServe(*answerAddr, nil))
+}
+
+func HandleClient(socketConn *websocket.Conn) {
+	fmt.Printf("Handling client\n")
+
+	localAddr := socketConn.LocalAddr()
+
+	fmt.Printf("localAddr: %s\n", localAddr)
+
+	socketConn.Close();
+
+	fmt.Printf("Connection closed\n")
 }
